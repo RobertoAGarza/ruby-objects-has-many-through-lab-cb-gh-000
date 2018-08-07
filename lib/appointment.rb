1ff34893dfb2c39d356ae47e67ff92c12db8c8c9
Roberto Garza <robertg4567@hotmail.com>
2018-08-07 14:23:42 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,14 +1,16 @@
 class Appointment
 
-  attr_accessor :name
+  attr_accessor :patient, :doctor, :date
   @@all = []
-  def initialize(name)
-    @name = name
+  def initialize(patient, doctor, date)
+    @patient = patient
+    @doctor = doctor 
+    @date = date
     @@all << self
   end
 
   def self.all
-    @@all 
+    @@all
   end
 
 end
