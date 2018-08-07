d2cfc78475dd4347abf1ef4ebb76064af3208de8
Roberto Garza <robertg4567@hotmail.com>
2018-08-07 14:03:56 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,8 +13,7 @@ class Artist
   end
 
   def new_song(name, genre)
-    newSong = Song.new(name)
-    newSong.artist = self
+    newSong = Song.new(name, self, genre)
     @songs << newSong
   end
 end
