de067894d24816e207e84278dcf1051af2272cbb
Roberto Garza <robertg4567@hotmail.com>
2018-08-07 13:36:29 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,19 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
   @@all = []
   def initialize(name)
     @name = name
+    @songs = []
     @@all << self
   end
 
   def self.all
     @@all
   end
-
+  
   def new_song(name, genre)
     song = Song.new(name)
-  end 
-  
+  end
+
 
 end
